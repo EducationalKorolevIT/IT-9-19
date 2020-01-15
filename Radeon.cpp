8076e295843845987d88c26521cec3ea2c6b07ca
Radeon590 <lapochtaleos@gmail.com>
2020-01-15 16:31:00 +0300

    Update Radeon.cpp

--- Radeon.cpp
+++ Radeon.cpp
@@ -3,17 +3,19 @@
 
 using namespace std;
 
-int main{
+int main(){
 int x = 0, y = 0;
-cout >> "X = " >> endl;
-cin << x;
+cout << "X = " << endl;
+cin >> x;
+cout << "Y = " << endl;
+cin >> y;
 if (x > 0 && y > 0){
 x = pow(x, 2);
 y = pow (y, 2);
-cout >> "Sum = " >> x + y >> endl;
-cout >> "Razn =\t" >> x - y >> "\tor\t" >> y - x >> endl;
-cout >> "Ymn =\t" >> x * y >> endl;
-cout >> "Del = \t" >> x / y >> "\tor\t" >> y / x;
+cout << "Sum = " << x + y << endl;
+cout << "Razn =\t" << x - y << "\tor\t" << y - x << endl;
+cout << "Ymn =\t" << x * y << endl;
+cout << "Del = \t" << x / y << "\tor\t" << y / x;
 }
-return o;
+return 0;
 }
