9504362d7d2c94d7e735b9beeca5734b52fc6d59
Stepan <59833944+Stepan358@users.noreply.github.com>
2020-01-15 16:15:15 +0300

    Create Stepan.cpp

---
+++ Stepan.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+int num1 = 0, num2 = 0;
+cin>> num1 >> num2;
+int num12 = num1 * num1, num22 = num2 * num2;
+if(num1 = 0){
+    cout << "error";
+}
+if(num2 = 0){
+    cout << "error";
+}
+cout<<num12 + num22<<endl<<num12 - num22<<endl<<num12 * num22<<endl<<num12 / num22;
+return 0;
+}
