3fe16c6162ada42613e30157353c65fc77582da1
HappyFox002 <45999287+HappyFox002@users.noreply.github.com>
2020-01-20 16:11:25 +0300

    Я сделял!

---
+++ testC#.cs
@@ -0,0 +1,12 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int a = 10, v = 20;
+		int c = int.Parse(Console.ReadLine());
+		//Console.WriteLine("Hello World");
+		Console.WriteLine(c);
+	}
+}
